b4b0bd3d15c538df116817e63e33fbcdf4301a92
SKJ APPS <support@skjapps.com>
2026-08-22 17:53:04 -0400

    mistbar 1.0.2 (build 9): auto-update hardening + Apps panel clipping fix

--- Casks/mistbar.rb
+++ Casks/mistbar.rb
@@ -1,6 +1,6 @@
 cask "mistbar" do
-  version "1.0.1"
-  sha256 "650757f933c25f44627c527b366fd6f46a9fb3fdd826ac9f174fe44f5886ebc3"
+  version "1.0.2"
+  sha256 "aef764afde1a1e52454a0d2f48e583b336b8d3e5c3e07b1fbc521437de8b1bc7"
 
   url "https://dl.mistbar.app/MistBar-#{version}.dmg",
       verified: "dl.mistbar.app/"
