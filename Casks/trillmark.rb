61c90a295bbcd44a162085efb47de7c4e4c8793f
SKJ APPS <dev@skjapps.com>
2026-08-26 22:49:24 -0400

    trillmark 1.0.4 (sha256 refresh)

--- Casks/trillmark.rb
+++ Casks/trillmark.rb
@@ -1,6 +1,6 @@
 cask "trillmark" do
-  version "1.0.3"
-  sha256 "edb61f677bac958ea3544e92a7b85f103de76f4ba866b532f0e1ababf7c55030"
+  version "1.0.4"
+  sha256 "3b86022a90637e3bb47750cf8519da30da9518a1ec601ce7bde9a86d52ec5e4e"
 
   url "https://dl.trillmark.app/TrillMark-#{version}.dmg",
       verified: "dl.trillmark.app/"
